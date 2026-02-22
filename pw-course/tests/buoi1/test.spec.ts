6124c890319bd2c9eff8a00f021b968af5e5c154
Nemo <nguyenthethanhphuong1209@gmail.com>
2026-02-22 17:03:50 +0700

    update

--- pw-course/tests/buoi1/test.spec.ts
+++ pw-course/tests/buoi1/test.spec.ts
@@ -3,9 +3,12 @@ test('has title', async ({ page }) => {
     await page.goto('http://material.playwrightvn.com/');
 
     await expect(page).toHaveTitle(/Tài liệu học automation test/);
+    await page.close();
 });
 
 test('get stated link', async ({ page }) => {
     await page.goto('http://material.playwrightvn.com/');
     await page.getByRole('link', { name: 'Bài học 1: Register Page (có đủ các element)' }).click();
+    await expect(page).toHaveTitle(/User Registration/);
+    await page.close();
 })
