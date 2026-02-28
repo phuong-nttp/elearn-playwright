af569916764f54f2bd26f45081c4d2579b88e78f
Nemo <nguyenthethanhphuong1209@gmail.com>
2026-02-28 14:48:35 +0700

    sua bai 1

--- pw-course/tests/buoi1/test.spec.ts
+++ pw-course/tests/buoi1/test.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
-    await page.goto('http://material.playwrightvn.com/');
+    await page.goto('https://material.playwrightvn.com/');
 
     await expect(page).toHaveTitle(/Tài liệu học automation test/);
     await page.close();
 });
 
 test('get stated link', async ({ page }) => {
-    await page.goto('http://material.playwrightvn.com/');
-    await page.getByRole('link', { name: 'Bài học 1: Register Page (có đủ các element)' }).click();
-    await expect(page).toHaveTitle(/User Registration/);
+    await page.goto('https://material.playwrightvn.com/');
+    await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
+    await expect(page.getByRole('heading', { name: 'User Registration' })).toHaveText(/User Registration/);
     await page.close();
 })
